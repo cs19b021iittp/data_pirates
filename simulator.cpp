86fc5997cb86fc182180b2e953d3247123f3e890
K-Ravi-Chandra <73428137+K-Ravi-Chandra@users.noreply.github.com>
2021-03-11 07:15:08 -0600

    Update simulator.cpp

--- simulator.cpp
+++ simulator.cpp
@@ -2,10 +2,42 @@
 # include <bits/stdc++.h>
 # include <fstream>
 # include <sstream> 
-
 using namespace std;
 
-int R[32];  // Global array of Registers
+int R[32];     // Global array of Registers
+int Mem[1024]; // Global array of Memory -> Creating array of 4KB memory = 4 bytes x ( 1024 length )
+int base_address;
+
+
+int Hexa_To_Dec_Converter(char num[])
+{
+   int len = strlen(num);
+   int base = 1;
+   int temp = 0;
+
+   for (int i=len-1; i>=0; i--)
+   {
+      if ( num[i]>='0' && num[i]<='9' ) 
+      {
+         temp += (num[i] - 48)*base;
+         base = base * 16;
+      }
+      else if ( num[i]>='A' && num[i]<='F' ) 
+      {
+         temp += (num[i] - 55)*base;
+         base = base*16;
+      }
+   }
+   return temp;
+}
+
+string Dec_To_Hexa_Converter(int num)
+{
+    char hex_string[20];
+    sprintf (hex_string, "%X", num); //convert number to hex
+
+    return hex_string;
+}
 
 
 void PerformAdd (char a,char b, char c,int x,int y, int z)
@@ -80,11 +112,10 @@ bool PerformEqual (char a,char b, int x,int y)
         return false;
 }
 
-
-
-bool Performslt (char a,char b, int x,int y)
+bool PerformSLT (char a,char b, char c,int x,int y, int z)
 {
     int p,q,r;
+
     if(a == 't')
         p = R[x];
     else if (a == 'r')
@@ -95,10 +126,97 @@ bool Performslt (char a,char b, int x,int y)
     else if (b == 'r')
         q = R[10+y];
 
-    if( p < q)
-        return true;
+    if(c == 't')
+        r = R[z];
+    else if (c == 'r')
+        r = R[10+z];
+
+    if(q < r)
+    {
+        if(a == 't')
+            R[x] = 1;
+        else if (a == 'r')
+            R[10+x] = 1;
+    }
+
     else
-        return false;
+    {
+        if(a == 't')
+            R[x] = 0;
+        else if (a == 'r')
+            R[10+x] = 0;
+    }
+    
+    return true;
+}
+
+
+bool PerformLI (char a, int x,int y)
+{
+    int p = y; // Storing the immediate integer to be stored
+
+    // Simply load that into the register
+    if(a == 't')
+        R[x] = p;
+    else if (a == 'r')
+        R[10+x] = p;
+
+    base_address = p;
+
+    return true;
+
+}
+
+bool PerformLW (char a,char b, int x,int y,int z)
+{
+    int q,r;
+
+    if (b == 't')
+        q = R[z];
+    else if (b == 'r')
+        q = R[10+z];
+
+    // Now q will be having address in decimal value
+    q = q + y;            // Adding offset
+    q = q - base_address; // Removing base address
+    q = q/4 ;             // To get index of Memory array 
+    
+    r = Mem[q];
+
+    //      int *p = &(Mem[1]);
+    //      cout << *(p);         so , p = 0x40d0a4 , *p = 2
+    
+    if (a == 't')
+        R[x] = r;
+    else if (a == 'r')
+        R[10+x] = r;
+
+    return true;
+}
+
+bool PerformSW (char a,char b, int x,int y,int z)
+{
+    int q,r;
+
+    if (b == 't')
+        q = R[z];
+    else if (b == 'r')
+        q = R[10+z];
+
+    // Now q will be having address in decimal value
+    q = q + y;            // Adding offset
+    q = q - base_address; // Removing base address
+    q = q/4 ;             // To get index of Memory array 
+
+    //      int *p = &(Mem[1]);
+    //      cout << *(p);         so , p = 0x40d0a4 , *p = 2
+    
+    if (a == 't')
+        Mem[q] = R[x] ;
+    else if (a == 'r')
+        Mem[q] = R[10+x] ;   
+
+    return true;
 }
 
 
@@ -250,6 +368,203 @@ bool addi_subi_Check ( string sentence, string word )
 }
 
 
+bool li_Check ( string sentence, string word )
+{
+    int x,y;
+    char a;
+
+    // To break the sentence in words 
+    stringstream s(sentence); 
+  
+    // To temporarily store each individual word 
+    string temp; 
+  
+    while (s >> temp) 
+    {   // Comparing the current word with the word to be searched 
+        if (temp.compare(word) == 0) 
+        {  
+            s >> temp;  // temp[0] = '$'
+            int p=1;           
+              
+            a = temp[p++];        // storing variable name of 1st register
+            x = temp[p++] - 48;   // storing value name of 1st register
+
+            s >> temp;  // temp[0] = '$'  
+                      
+            if( temp[1] == 'x' )  // If that number is in hexadecimal
+            {
+                char hex[1000000] ;
+
+                for(int d=2; temp[d]!=' ' ; d++)
+                hex[d-2] = temp[d];
+
+                // cout << a << endl;   
+                // Now this string 'a' will be storing the part after '0x' in hexadecimal input given
+
+                // I will convert this 'hexadecimal string' into 'decimal int' and store in register array
+                int int_temp = int_temp = Hexa_To_Dec_Converter( hex );
+                
+                y = int_temp;   // storing integer value of 3rd register
+
+                PerformLI (a,x,y);
+
+            }
+
+            else  // if that number is in Decimal value
+            {
+                string str_temp = temp; 
+                stringstream geek(str_temp); // object from the class stringstream 
+  
+                // The object has integer in string form and now, stream it to the integer int_temp 
+                int int_temp = 0; 
+                geek >> int_temp; 
+  
+                // Now the variable int_temp holds the integer value of the string        
+              
+                y = int_temp;   // storing integer value of 3rd register
+            
+                PerformLI (a,x,y);
+            }
+            
+
+            return true; 
+        }
+           
+            
+    } 
+    return false; 
+}
+
+bool lw_Check ( string sentence, string word )
+{
+    int x,y,z;
+    char a,b;
+
+    stringstream s(sentence); 
+    string temp; 
+  
+    while (s >> temp) 
+    {  
+        if (temp.compare(word) == 0) 
+        {  
+            s >> temp;  // temp[0] = '$'
+            int p=1;           
+              
+            a = temp[p++];        // storing variable name of 1st register
+            x = temp[p++] - 48;   // storing value name of 1st register
+
+            s >> temp;   
+           
+            string offset = "";     
+            int k=0;       
+            for( k=0 ; temp[k]!='(' ; k++)
+            {
+                offset += temp[k];     // collecting offset as a string
+                //offset[i] = temp[k]; //This method is not working
+            }
+
+            stringstream geek(offset);  
+            geek >> y;            // converting offset into int
+           
+            p = k+2;   
+            b = temp[p++];        // storing variable name of 2nd register 
+            z = temp[p++] - 48;   // storing value name of 2nd register 
+
+            PerformLW (a,b,x,y,z); 
+               
+            return true; 
+        }
+        else
+            return false;
+    } 
+}
+
+bool sw_Check ( string sentence, string word )
+{
+    int x,y,z;
+    char a,b;
+
+    stringstream s(sentence); 
+    string temp; 
+  
+    while (s >> temp) 
+    {  
+        if (temp.compare(word) == 0) 
+        {  
+            s >> temp;  // temp[0] = '$'
+            int p=1;           
+              
+            a = temp[p++];        // storing variable name of 1st register
+            x = temp[p++] - 48;   // storing value name of 1st register
+
+            s >> temp;   
+           
+            string offset = "";     
+            int k=0;       
+            for( k=0 ; temp[k]!='(' ; k++)
+            {
+                offset += temp[k];     // collecting offset as a string
+                //offset[i] = temp[k]; //This method is not working
+            }
+
+            stringstream geek(offset);  
+            geek >> y;            // converting offset into int
+           
+            p = k+2;   
+            b = temp[p++];        // storing variable name of 2nd register 
+            z = temp[p++] - 48;   // storing value name of 2nd register 
+
+            PerformSW (a,b,x,y,z); 
+               
+            return true; 
+        }
+        else
+            return false;
+    } 
+}
+
+
+bool slt_Check ( string sentence, string word )
+{
+    int  x,y,z;
+    char a,b,c;
+
+    stringstream s(sentence); 
+    string temp; 
+  
+    while (s >> temp) 
+    {  
+        if (temp.compare(word) == 0) 
+        {  
+            s >> temp;  // temp[0] = '$'
+            int p=1;           
+              
+            a = temp[p++];        // storing variable name of 1st register
+            x = temp[p++] - 48;   // storing value name of 1st register
+
+
+            s >> temp;  // temp[0] = '$'  
+            p=1;           
+              
+            b = temp[p++];        // storing variable name of 2nd register
+            y = temp[p++] - 48;   // storing value name of 2nd register
+
+
+            s >> temp;  // temp[0] = '$'  
+            p=1;           
+              
+            c = temp[p++];        // storing variable name of 3rd register
+            z = temp[p++] - 48;   // storing value name of 3rd register
+
+            PerformSLT (a,b,c,x,y,z);
+            
+            return true; 
+        }
+        else
+            return false;
+    } 
+}
+
 string jump_Check ( string sentence, string word )
 { 
     stringstream s(sentence); 
@@ -330,10 +645,17 @@ string search_label(string str)
     } 
 } 
 
-void PrintAllRegisters(int R[] , int k)
+
+void PrintAllRegisters (int R[] , int k)
 {
     for(int i=0;i<k;i++)
         cout << " R[" << i << "] = " << R[i] << endl;
+}
+
+void PrintAllMemory ( int Mem[] , int k )
+{
+    for(int i=0;i<k;i++)
+        cout << " Mem[" << i << "] = " << Mem[i] << endl;
 }
 
 
@@ -342,11 +664,21 @@ int main()
     int n = 10;      // file size
     string arr[n];   // Each one stores one line of file
 
-    int i=0;
+    // Assigning values to registers
+    int i=0;         
     for(int i=0;i<32;i++)
        R[i] = i;
 
-    ////***********************************************////
+    // Creating array of 4KB memory = 4 bytes x ( 1024 length )
+    for (int i=0;i<1024;i++)
+        Mem[i] = 2 * i;
+   
+
+    // Here we can clearly observe that this array holds fixed memory address in all cases
+    // cout << Mem[0] << endl <<  &(Mem[0]) << endl << *(&(Mem[0])) << endl;
+    // cout << Memory[1] << endl <<  &(Memory[1]) << endl;
+
+    ////***********************************************//// 
 
     std :: string line;
     ifstream file("assembly.txt");
@@ -379,14 +711,28 @@ int main()
 
     for (int k=0;k<n;k++)
     {  
+        // if(arr[k] == "")
+        // cout << "* ";      // This will print only if a line dont have anything (not even space)
+        //                       or in the case of  the last unused remaining lines
+
+        if(arr[k] == "")
+        k++;
+
         add_sub_Check  ( arr[k], "add" ) ;
         add_sub_Check  ( arr[k], "sub" ) ;
             
         addi_subi_Check( arr[k], "addi" ) ;
         addi_subi_Check( arr[k], "subi" ) ;
 
-       
-        string jumpLabel = jump_Check(arr[k], "j");
+        li_Check ( arr[k], "li" );
+        lw_Check ( arr[k], "lw" );
+        sw_Check ( arr[k], "sw" );
+
+        slt_Check( arr[k], "slt");
+
+
+        // Checking 'J'
+        string jumpLabel = jump_Check(arr[k], "j");   
         if( arr[k]!= ""  &&  jump_Check(arr[k], "j") != "fault" )
         {
             // jumpLabel = jumpLabel +":";     //should not do
@@ -399,6 +745,7 @@ int main()
             continue;
         }
 
+        // Checking 'bne'
         string  bne_Label =  bne_Check (arr[k], "bne");
         if( arr[k]!= ""  &&  bne_Check(arr[k], "bne") != "fault" )
         {
@@ -410,16 +757,28 @@ int main()
 
     }
 
-    PrintAllRegisters(R, 32);
+    cout << "\n  Register Elements :" << "\n\n";
+    PrintAllRegisters(R, 32);   // Printing 32 registers
+
+    cout << "\n  Memory Elements :" << "\n\n";
+    PrintAllMemory (Mem, 15);   // Printing required number of memory elements upto 1024
+
 
     file.close();
     return 0;
 }
 
+/*  EXCEPTIONS 
+  
+    -> should follow good pattern or spacing in add ,addi,.....
+    -> file should not be empty
+    -> each line should end with space
+    -> we have 20 registers at present
+    -> If u find any other include here
+*/
 
 
-/* Test file
-
+/* Test file to check j and bne
 
 add $t0, $t1, $t2 
 add $t3, $t4, $t5 
